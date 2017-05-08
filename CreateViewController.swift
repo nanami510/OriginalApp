1ae4c8348184b9d5f292b1d0051ce25dcba38fa7
後藤奈々美 <gotonanami0510@yahoo.co.jp>
2017-05-08 13:03:56 +0900

    テーブル追加

--- CreateViewController.swift
+++ CreateViewController.swift
@@ -8,34 +8,74 @@
 
 import UIKit
 import RealmSwift
+
 class Todo: Object {
+    dynamic var todo_id = ""
     dynamic var title = ""
     dynamic var date = ""
+    dynamic var starttime = ""
+    dynamic var endtime = ""
+    dynamic var memo = ""
+    }
+
+class TimeTable: Object {
+    dynamic var timetable_id = ""
+    dynamic var title = ""
+    dynamic var dayOfTheWeek = ""
+    dynamic var period=""
     
+}
+
+class Note:Object{
+    dynamic var note_id = ""
+    dynamic var title = ""
+    dynamic var dayOfTheWeek = ""
+    dynamic var period=""
+    dynamic var date = ""
 }
 
 
 class CreateViewController: UIViewController, UITextFieldDelegate {
-    
+    @IBOutlet var date:UILabel!
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var start: UITextField!
+    @IBOutlet weak var end: UITextField!
+    @IBOutlet weak var textView:UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        date.text = selectedDay
+        start.delegate = self
+        end.delegate = self
         textField.delegate = self
+        textField.returnKeyType = .done
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(textFieldDidChange),
+            name: NSNotification.Name.UITextFieldTextDidChange,
+            object: textField)
+        textView.dataDetectorTypes = UIDataDetectorTypes.all
+        textView.isEditable = true
     }
-    
-    
-    
-    
-    
-    
     
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc private func textFieldDidChange(notification: NSNotification) {
+        let textFieldString = notification.object as! UITextField
+        if let text = textFieldString.text {
+            if text.characters.count > 8 {
+                textField.text = text.substring(to: text.index(text.startIndex, offsetBy: 3))
+            }
+        }
+    }
     
     @IBAction func createButton(_sender: Any) {
         let realm = try! Realm()
@@ -43,20 +83,15 @@ class CreateViewController: UIViewController, UITextFieldDelegate {
         let todo = Todo()
         todo.title = textField.text!
         todo.date = selectedDay!
+        todo.starttime = start.text!
+        todo.endtime = end.text!
+        
+        todo.memo = textView.text
         try! realm.write {
             realm.add(todo)
         }
         self.dismiss(animated: true, completion: nil)
     }
-    
-    
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.textField.resignFirstResponder()
-        return true
-    }
-    
-    
     
     
 }
